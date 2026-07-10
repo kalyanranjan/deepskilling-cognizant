3e8e651094a239a8c3d501b0bd3b0d88f008c8ef
Kalyan Ranjan <kalyanvranjan@gmail.com>
2026-07-10 22:39:13 +0530

    (module-2, hands-on-6) feat: implement many-to-many relationship between employee and skill

---
+++ Spring Data JPA & Hibernate/spring-data-jpa-module-2/hands_on_6/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,62 @@
+package com.cognizant.ormlearn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.ormlearn.model.Department;
+import com.cognizant.ormlearn.model.Employee;
+import com.cognizant.ormlearn.model.Skill;
+import com.cognizant.ormlearn.service.DepartmentService;
+import com.cognizant.ormlearn.service.EmployeeService;
+import com.cognizant.ormlearn.service.SkillService;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+    
+    private static EmployeeService employeeService;
+    private static DepartmentService departmentService;
+    private static SkillService skillService;
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+        
+        employeeService = context.getBean(EmployeeService.class);
+        departmentService = context.getBean(DepartmentService.class);
+        skillService = context.getBean(SkillService.class);
+        
+        // testGetEmployee();
+        // testGetDepartment();
+        testAddSkillToEmployee();
+    }
+
+    private static void testGetEmployee() {
+        LOGGER.info("Start");
+        Employee employee = employeeService.get(1);
+        LOGGER.debug("Employee:{}", employee);
+        LOGGER.debug("Department:{}", employee.getDepartment());
+        LOGGER.debug("Skills:{}", employee.getSkillList());
+        LOGGER.info("End");
+    }
+
+    private static void testGetDepartment() {
+        LOGGER.info("Start");
+        Department department = departmentService.get(1);
+        LOGGER.debug("Department:{}", department);
+        LOGGER.debug("Employees:{}", department.getEmployeeList());
+        LOGGER.info("End");
+    }
+
+    private static void testAddSkillToEmployee() {
+        LOGGER.info("Start");
+        Employee employee = employeeService.get(1);
+        Skill skill = skillService.get(3);
+        employee.getSkillList().add(skill);
+        employeeService.save(employee);
+        LOGGER.info("End");
+    }
+}
