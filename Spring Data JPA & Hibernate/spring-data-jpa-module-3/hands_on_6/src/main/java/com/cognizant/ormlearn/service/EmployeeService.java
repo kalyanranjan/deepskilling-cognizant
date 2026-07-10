13c0e88a63ed0103f5af2a67447f52149a8d1b9f
Kalyan Ranjan <kalyanvranjan@gmail.com>
2026-07-10 22:40:42 +0530

    (module-3, hands-on-6) feat: search employees dynamically using JPA criteria query

---
+++ Spring Data JPA & Hibernate/spring-data-jpa-module-3/hands_on_6/src/main/java/com/cognizant/ormlearn/service/EmployeeService.java
@@ -0,0 +1,35 @@
+package com.cognizant.ormlearn.service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cognizant.ormlearn.model.Employee;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeService.class);
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
+    public List<Employee> getAllEmployeesCriteria() {
+        LOGGER.info("Start");
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Employee> cq = cb.createQuery(Employee.class);
+        Root<Employee> root = cq.from(Employee.class);
+        cq.select(root);
+        return entityManager.createQuery(cq).getResultList();
+    }
+}
