91e0cfe11b1699fe04a8d34a769ca1eebbbb11ca
Kalyan Ranjan <kalyanvranjan@gmail.com>
2026-07-16 06:04:27 +0530

    feat: add eureka discovery server, api gateway, and register microservices

---
+++ Microservices/api-gateway/src/main/java/com/cognizant/apigateway/LogFilter.java
@@ -0,0 +1,22 @@
+package com.cognizant.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LogFilter implements GlobalFilter {
+
+    private static final Logger log = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Incoming Request: {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+
+}
